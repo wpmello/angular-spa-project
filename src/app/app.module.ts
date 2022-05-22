9f288fdaf2a2e9c0053e65e96de93750e5e31c78
wpmello <wagnermello903@gmail.com>
2022-05-22 00:26:52 -0300

    Filtering books by category - Add toolbar & table components thought angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSelectModule} from '@angular/material/select';
+import { TableComponent } from './table/table.component';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ToolbarComponent,
+    TableComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatTableModule,
     BrowserAnimationsModule
   ],
   providers: [],
